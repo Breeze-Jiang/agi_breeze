04e00d9b306c1e7d05943757056da3c2b7ced6c8
BreezeJiang <3833589331@qq.com>
2026-06-14 23:03:27 +0800

    1

--- jzh_ai/fe/js/type/1.js
+++ jzh_ai/fe/js/type/1.js
@@ -1,4 +1,25 @@
 // 表示空或者没有
 //null
+//primitive 原始 内存空间固定
 let a = null;
+let b = a;
+b = 2;
 console.log(a);
+console.log(b);
+
+let obj = {
+  name: 'zhansan',
+  address: null
+}
+let obj2 = obj;
+obj2.company = 'alibaba';
+console.log(obj,obj2);
+// console.log(obj.address);
+// console.log(obj.age);
+
+// let largeObject = {
+//   data:new Array(1000000).fill("hgh")
+// }
+// //手动回收
+// largeObject = null;
+// console.log(largeObject);
---
+++ jzh_ai/fe/js/type/2.js
@@ -0,0 +1,12 @@
+let a;// 未初始化 undefined
+console.log(a);
+let obj = {};
+console.log(obj.property);
+function fn() {
+  
+}
+fn(); //没有返回的函数 返回undefined
+
+let arr = [];
+console.log(arr[100]);
+// undefined
